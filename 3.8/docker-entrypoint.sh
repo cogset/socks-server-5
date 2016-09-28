b7cad997813b1df56d7cfbe9ac1cc488056ea5a1
You Ming <youming@funcuter.org>
2016-09-28 11:24:12 +0800

    Added version 3.8.

---
+++ 3.8/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -n "$SS_USER" -a -n "$SS_PASS" ]
+then
+    echo "$SS_USER $SS_PASS" > /etc/opt/ss5/ss5.passwd
+fi
+
+ss5 -t
+
+trap "kill `cat /var/run/ss5/ss5.pid`; exit" KILL INT
+
+"$@"
+
+wait
